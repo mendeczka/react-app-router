b439427c055e4d06134ae0fa591eb847f5274076
mendeczka <mendeczka@o2.pl>
2022-03-20 12:00:15 +0100

    Updates

---
+++ precache-manifest.0a58f876278584b301d9a1e5e11ec949.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "6139c4d76962d3457165",
+    "url": "/react-app-router/static/css/main.09c0117b.chunk.css"
+  },
+  {
+    "revision": "6139c4d76962d3457165",
+    "url": "/react-app-router/static/js/main.7011da09.chunk.js"
+  },
+  {
+    "revision": "90b39a9c809bee6964c3",
+    "url": "/react-app-router/static/js/runtime~main.13db28db.js"
+  },
+  {
+    "revision": "15023937afdb327f75cc",
+    "url": "/react-app-router/static/js/2.d67f3671.chunk.js"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/react-app-router/static/media/header3.d9d36b65.jpg"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/react-app-router/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/react-app-router/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "7bf162149e966e59e305a4d3b23fcd70",
+    "url": "/react-app-router/index.html"
+  }
+];
